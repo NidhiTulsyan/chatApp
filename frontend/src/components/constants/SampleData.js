f334362e8ac3c524bb6a27ad314e09ed89321b85
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2024-03-21 15:23:51 +0530

    completed search new group notifcations

--- frontend/src/components/constants/SampleData.js
+++ frontend/src/components/constants/SampleData.js
@@ -1,16 +1,50 @@
 export const sampleData = [
   {
-    avatar: ["https://www.w3schools.com/w3images/avatar5.png"],
+    avatar: ["https://www.w3schools.com/w3images/avatar1.png"],
     name: "john doe",
     _id: "1",
     groupChat: false,
     members: ["1", "2"],
   },
   {
-    avatar: ["https://www.w3schools.com/w3images/avatar5.png"],
+    avatar: ["https://www.w3schools.com/w3images/avatar3.png"],
     name: "john noi",
     _id: "2",
     groupChat: false,
     members: ["1", "2"],
   },
 ];
+
+export const sampleUsers = [
+  {
+    avatar: ["https://www.w3schools.com/w3images/avatar2.png"],
+    name: "paul doe",
+    _id: "1",
+   
+  },
+  {
+    avatar: ["https://www.w3schools.com/w3images/avatar3.png"],
+    name: "abby singh",
+    _id: "2",
+   
+  },
+ 
+]
+
+export const sampleNotifications = [
+  {
+    sender:{
+      avatar: ["https://www.w3schools.com/w3images/avatar4.png"],
+      name: "paul doe",
+    },
+    _id: "1",
+  },
+  {
+    sender:{
+      avatar: ["https://www.w3schools.com/w3images/avatar5.png"],
+      name: "abby singh",
+    },
+    _id: "2",
+  },
+  
+]
--- frontend/src/components/specific/Search.jsx
+++ frontend/src/components/specific/Search.jsx
@@ -1,16 +1,25 @@
 import { useInputValidation } from "6pp";
-import { Search as SearchIcon} from "@mui/icons-material";
+import { Search as SearchIcon } from "@mui/icons-material";
 import {
   Dialog,
   DialogTitle,
   InputAdornment,
+  List,
   Stack,
   TextField,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import UserItem from "../shared/UserItem";
+import { sampleUsers } from "../constants/SampleData";
 
+// const users = [1, 2, 3, 4];
 const Search = () => {
   const search = useInputValidation("");
+  const [users, setUsers] = useState(sampleUsers);
+  let isLoadingSendFriendRequest = false;
+  const addFriendHandler = (id) => {
+    console.log(id);
+  };
   return (
     <Dialog open>
       <Stack p={"2rem"} direction={"column"} width={"25rem"}>
@@ -29,6 +38,16 @@ const Search = () => {
             ),
           }}
         />
+        <List>
+          {users.map((i) => (
+            <UserItem
+              user={i}
+              key={i._id}
+              handler={addFriendHandler}
+              handlerIsLoading={isLoadingSendFriendRequest}
+            />
+          ))}
+        </List>
       </Stack>
     </Dialog>
   );
